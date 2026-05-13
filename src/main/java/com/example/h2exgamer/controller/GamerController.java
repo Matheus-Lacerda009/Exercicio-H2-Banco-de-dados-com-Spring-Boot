557fdc9fa29313fab6f70dd88abd5257064e5c02
Matheus Lacerda <matheuslacerda.mala@gmail.com>
2026-05-13 12:22:30 -0300

    Correção de escrita em um @RequestMapping

--- src/main/java/com/example/h2exgamer/controller/GamerController.java
+++ src/main/java/com/example/h2exgamer/controller/GamerController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/alunos")
+@RequestMapping("/gamer")
 @RequiredArgsConstructor
 public class GamerController {
     private final GamerService service;
